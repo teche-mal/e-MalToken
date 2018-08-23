13d3f2e07f75b7c3ed449957a15895d6c68d7111
e-MAL <38875465+teche-mal@users.noreply.github.com>
2018-08-23 12:20:46 +0400

    Updated contract code.

--- EmalWhitelist.sol
+++ EmalWhitelist.sol
@@ -1,32 +1,59 @@
-pragma solidity ^0.4.11;
+pragma solidity 0.4.24;
 
+import './Ownable.sol';
 
-/**
- * This contract provides support for whitelisting addresses
+/** @notice This contract provides support for whitelisting addresses.
+ * only whitelisted addresses are allowed to send ether and buy tokens
+ * during preSale and Pulic crowdsale.
+ * @dev after deploying contract, deploy Presale / Crowdsale contract using
+ * EmalWhitelist address. To allow claim refund functionality and allow wallet
+ * owner efatoora to send ether to Crowdsale contract for refunds add wallet
+ * address to whitelist.
  */
-contract EmalWhitelist {
+contract EmalWhitelist is Ownable {
 
-  mapping(address => bool) public whitelist;
+    mapping(address => bool) whitelist;
 
-  /**
-   * Returns if an address is whitelisted or not
-   */
-  function isWhitelisted(address investorAddr) public view returns (bool whitelisted) {
-    return whitelist[investorAddr];
-  }
+    event AddToWhitelist(address investorAddr);
+    event RemoveFromWhitelist(address investorAddr);
 
-  /**
-   * Adds an address to whitelist
-   */
-  function addToWhitelist(address investorAddr) public {
-    whitelist[investorAddr] = true;
-  }
 
-  /**
-   * Removes an address from whitelist
-   */
-  function removeFromWhitelist(address investorAddr) public {
-    whitelist[investorAddr] = false;
-  }
+    /** @dev Throws if operator is not whitelisted.
+     */
+    modifier onlyIfWhitelisted(address investorAddr) {
+        require(whitelist[investorAddr]);
+        _;
+    }
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    /** @dev Returns if an address is whitelisted or not
+     */
+    function isWhitelisted(address investorAddr) public view returns(bool whitelisted) {
+        return whitelist[investorAddr];
+    }
+
+    /**
+     * @dev Adds an investor to whitelist
+     * @param investorAddr The address to user to be added to the whitelist, signifies that the user completed KYC requirements.
+     */
+    function addToWhitelist(address investorAddr) public onlyOwner returns(bool success) {
+        require(investorAddr!= address(0));
+        whitelist[investorAddr] = true;
+        return true;
+    }
+
+    /**
+     * @dev Removes an investor's address from whitelist
+     * @param investorAddr The address to user to be added to the whitelist, signifies that the user completed KYC requirements.
+     */
+    function removeFromWhitelist(address investorAddr) public onlyOwner returns(bool success) {
+        require(investorAddr!= address(0));
+        whitelist[investorAddr] = false;
+        return true;
+    }
+
 
 }
